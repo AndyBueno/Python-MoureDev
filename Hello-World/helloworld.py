08ab497436aba658213828ce32e6abaa1fce41ed
Andres Bueno Alvarado <andybueno@MacBook-Air-de-Andres.local>
2024-06-02 23:10:27 -0700

    Fix: Add new examples to helloworld file

--- Hello-World/helloworld.py
+++ Hello-World/helloworld.py
@@ -1,2 +1,27 @@
 #Print hello world with python
 print('Hello World - Andy Bueno')
+"""
+    Este es un comentario 
+    en varias lineas
+"""
+
+'''
+    Este tambien es un comentario
+    en varias lineas
+'''
+
+#Consultar el tipo de dato
+saludo = "Hola Andy"
+print(type(saludo)) #Tipo de dato STR
+
+numEjemplo = 1
+print(type(numEjemplo)) #Tipo de dato Numerico
+
+numPi = 3.1415
+print(type(numPi)) #Tipo de dato float
+
+boolDecision = True
+print(type(boolDecision)) #Tipo de dato bool
+
+datoComplejo = 3 + 1j
+print(type(datoComplejo))
